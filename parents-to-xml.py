3f3d74631927fe9c8a852eec74760395cd6f5c99
robertcope <cope.robert.c@gmail.com>
2013-12-03 13:33:18 +1000

    parents-to-xml edit

--- parents-to-xml.py
+++ parents-to-xml.py
@@ -26,27 +26,6 @@ inFile = open(infile,'r')
 print infile
 outFile = open(filedir + 'TEMP'+sys.argv[1]+'.txt','w')
 
-#
-#    Copyright 2013 Robert Cope cope.robert.c@gmail.com
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-#
-
-
-
 matchingRegex = re.compile("\s{0,20}(\d{1,6})\ {1,4}\(\d{0,6}\s{0,4}\d{0,6}\)\ {1,4}(\d{1,2})(?:\ {1,4}(\d{1,4})\ {1,4}(\d{1,4}))?\ {1,4}([\?0-9MF]{1,4})\ {1,4}([\?0-9]{1,4})(?:\ {1,4}([\?0-9]{1,4}))?((?:\ {1,8}[\?0-9]{1,8}\.[\?0-9]{1,8}){1,100})")
 print >> outFile, "1 25 . temp"
 for i in range(25):
